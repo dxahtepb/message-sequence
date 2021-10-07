cb51eb35e201c16356ea61b4f3b9b0ce8f31e23c
Andrey Chausov <dxahtepb@gmail.com>
2021-10-07 21:22:16 +0300

    auto-format

--- src/ts/Settings.ts
+++ src/ts/Settings.ts
@@ -8,60 +8,60 @@ import {
 } from "./Constants";
 
 export interface Settings {
-    isColorizeTraces: boolean;
-    timeScale: TimeScale;
-    dataFilePath: string;
-    readableFilePath: string;
+  isColorizeTraces: boolean;
+  timeScale: TimeScale;
+  dataFilePath: string;
+  readableFilePath: string;
 
-    apply(): void;
+  apply(): void;
 }
 
 export const settings: Settings = {
-    isColorizeTraces: DEFAULT_COLORIZE_TRACES,
-    timeScale: DEFAULT_TIME_SCALE,
-    dataFilePath: DEFAULT_TRACE,
-    readableFilePath: DEFAULT_TRACE_FILENAME,
-    apply: () => {
-        settings.isColorizeTraces = document.querySelector<HTMLInputElement>(
-            "input[id=colorize-traces]"
-        )?.checked ?? DEFAULT_COLORIZE_TRACES;
-        const timeScaleSelectorValue = document.querySelector<HTMLInputElement>(
-          'input[name="time-scale-selector"]:checked'
-        )?.value ?? DEFAULT_TIME_SCALE
-        settings.timeScale = timeScaleSelectorValue === TimeScale.LOGICAL
-          ? TimeScale.LOGICAL
-          : TimeScale.REAL;
-        const files = document.querySelector<HTMLInputElement>("[id=file-selector]")?.files;
-        if (files != null && files.length !== 0) {
-            settings.readableFilePath = files[0].name;
-            settings.dataFilePath = URL.createObjectURL(files[0]);
-        }
-        console.log(settings);
-    },
+  isColorizeTraces: DEFAULT_COLORIZE_TRACES,
+  timeScale: DEFAULT_TIME_SCALE,
+  dataFilePath: DEFAULT_TRACE,
+  readableFilePath: DEFAULT_TRACE_FILENAME,
+  apply: () => {
+    settings.isColorizeTraces = document.querySelector<HTMLInputElement>(
+      "input[id=colorize-traces]"
+    )?.checked ?? DEFAULT_COLORIZE_TRACES;
+    const timeScaleSelectorValue = document.querySelector<HTMLInputElement>(
+      'input[name="time-scale-selector"]:checked'
+    )?.value ?? DEFAULT_TIME_SCALE
+    settings.timeScale = timeScaleSelectorValue === TimeScale.LOGICAL
+      ? TimeScale.LOGICAL
+      : TimeScale.REAL;
+    const files = document.querySelector<HTMLInputElement>("[id=file-selector]")?.files;
+    if (files != null && files.length !== 0) {
+      settings.readableFilePath = files[0].name;
+      settings.dataFilePath = URL.createObjectURL(files[0]);
+    }
+    console.log(settings);
+  },
 };
 
 document.querySelector<HTMLButtonElement>("button[id=settings-button]")
-    ?.addEventListener("click", () => {
-        const settingsButton = document.querySelector<HTMLButtonElement>("button[id=settings-button]");
-        const settings = d3.select("#settings");
-        if (settingsButton == null || settings == null) {
-            throw "Cannot get settings div or settings button"
-        }
-        if (settings.attr("show") === "true") {
-            settingsButton.textContent = "Open Settings";
-            settings
-                .attr("show", false)
-                .style("pointer-events", "none")
-                .transition()
-                .duration(300)
-                .style("opacity", 0);
-        } else {
-            settingsButton.textContent = "Close Settings";
-            settings
-                .attr("show", true)
-                .style("pointer-events", "auto")
-                .transition()
-                .duration(300)
-                .style("opacity", 1);
-        }
-    });
+  ?.addEventListener("click", () => {
+    const settingsButton = document.querySelector<HTMLButtonElement>("button[id=settings-button]");
+    const settings = d3.select("#settings");
+    if (settingsButton == null || settings == null) {
+      throw "Cannot get settings div or settings button"
+    }
+    if (settings.attr("show") === "true") {
+      settingsButton.textContent = "Open Settings";
+      settings
+        .attr("show", false)
+        .style("pointer-events", "none")
+        .transition()
+        .duration(300)
+        .style("opacity", 0);
+    } else {
+      settingsButton.textContent = "Close Settings";
+      settings
+        .attr("show", true)
+        .style("pointer-events", "auto")
+        .transition()
+        .duration(300)
+        .style("opacity", 1);
+    }
+  });
