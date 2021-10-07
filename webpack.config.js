ba6a5f17f765f82afdc4a76a5cbe04146b4ea5f5
Andrey Chausov <dxahtepb@gmail.com>
2021-10-07 18:53:12 +0300

    Replace parcel with webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    hot: true,
+  },
+};
